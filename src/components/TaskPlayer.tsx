aefbe17b9f28a7f89d0d948e46d8fa22a9a40c97
Lucas Sousa <ls442336@gmail.com>
2023-03-05 10:28:38 -0300

    Add ToggleButton

--- src/components/TaskPlayer.tsx
+++ src/components/TaskPlayer.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
-const TaskPlayer: React.FC<any> = () => {
+import ToggleButton from './ToggleButton'
+import { formatTime } from '../utils'
+
+interface TaskPlayerProps {
+  time?: number
+}
+
+const TaskPlayer: React.FC<TaskPlayerProps> = ({ time }) => {
+  const formattedTime = formatTime(time ?? 0)
+
   return (
     <div className="grid-cols-1 gap-9 grid rounded-lg py-6 bg-white bg-opacity-25">
       <div className="text-9xl leading-none font-semibold text-center text-white">
-        05:00
+        {formattedTime}
       </div>
 
       <div className="justify-center flex">
-        <button
-          type="button"
-          className="rounded-md shadow-[rgb(209,213,219)_0px_6px_0px] mb-[6px] min-w-[12.5rem] py-3 px-3 bg-white text-xl font-bold uppercase text-sky-700 active:translate-y-[6px] active:shadow-none"
-        >
-          start
-        </button>
+        <ToggleButton activeText="pause" inactiveText="start" />
       </div>
     </div>
   )
---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+interface ToggleButtonProps {
+  activeText?: string
+  inactiveText?: string
+  onClick?: () => void
+  onActive?: () => void
+  onInactive?: () => void
+}
+
+const BASE_CLASS_NAME = `rounded-md 
+shadow-[rgb(209,213,219)_0px_6px_0px] 
+mb-[6px] 
+min-w-[12.5rem] 
+py-3 
+px-3 
+bg-white 
+text-xl 
+font-bold 
+uppercase 
+text-sky-700`
+
+const ACTIVE_CLASS_NAME = `translate-y-[6px] shadow-none`
+
+const ToggleButton: React.FC<ToggleButtonProps> = ({
+  activeText,
+  inactiveText,
+  onClick,
+  onActive,
+  onInactive,
+}) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const activeClassName = (): string => {
+    if (!isActive) {
+      return ''
+    }
+
+    return ACTIVE_CLASS_NAME
+  }
+
+  const className = (): string => {
+    console.log(`${BASE_CLASS_NAME} ${activeClassName()}`)
+
+    return `${BASE_CLASS_NAME} ${activeClassName()}`
+  }
+
+  const handleClick = (): void => {
+    setIsActive(!isActive)
+    onClick?.()
+    isActive ? onInactive?.() : onActive?.()
+  }
+
+  return (
+    <button type="button" className={className()} onClick={handleClick}>
+      {isActive ? activeText : inactiveText}
+    </button>
+  )
+}
+
+export default ToggleButton
